8d30ffd751da639e17db22aa048bcb31a822b697
aamir98748 <aamirzeeshaan@gmail.com>
2022-05-31 16:38:42 +0300

    d

---
+++ Salam السلام/Salam السلام/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Salam السلام
+//
+//  Created by Zee  on 3/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var LoginView: UIView!
+    
+    @IBOutlet weak var passwordTxtField: UITextField!
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var SignBtn: UIButton!
+    
+    @IBOutlet weak var passwordBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
